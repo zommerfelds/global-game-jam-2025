8bb0195e745e09ab6f70cb3d7c935487f94c2022
Karolina Flendrich <czaplakaro@gmail.com>
2025-01-24 21:28:56 +0100

    Implement the area-painting logic
    The area within the canvas is painted in the chosen color.

--- scripts/paintable_area.gd
+++ scripts/paintable_area.gd
@@ -1,22 +1,35 @@
 extends Sprite2D
 
+var border_size = 10
+var area_width = 600
+var area_height = 500
+var splash_size = 20
+var image = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	var border_size = 10
-	var area_width = 600
-	var area_height = 500
-	var image = Image.create(area_width, area_height, false, Image.FORMAT_RGBA8)
+	image = Image.create(area_width, area_height, false, Image.FORMAT_RGBA8)
 	image.fill(Color.DARK_SLATE_BLUE)
 	image.fill_rect(
 		Rect2i(border_size, border_size, area_width - 2*border_size, area_height - 2*border_size),
-	 	Color.LAVENDER)
+	 	Color.LAVENDER
+	)
 	set_position(Vector2(400, 300))
 	texture = ImageTexture.create_from_image(image)
-
-	pass # Replace with function body.
-
+	
+	# A test for painting a certain area.
+	_paint_area(Vector2(200, 200), Color.HOT_PINK)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+	
+func _paint_area(position: Vector2, color: Color) -> void:
+	image.fill_rect(
+		Rect2i(position.x, position.y, splash_size, splash_size),
+		color
+	)
+	texture = ImageTexture.create_from_image(image)
+	
