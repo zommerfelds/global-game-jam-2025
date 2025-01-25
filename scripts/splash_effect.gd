bf1dde65508064149acaf28766da395183a20ead
Yanick Senn <ys+github@yanicksenn.com>
2025-01-25 21:04:08 +0100

    Fix shader for splash effect

--- scripts/splash_effect.gd
+++ scripts/splash_effect.gd
@@ -5,5 +5,6 @@ var color: Color = Color.WHITE
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	# TODO fix that it does not override the color globally on the material...
+	$GPUParticles2D.process_material = $GPUParticles2D.process_material.duplicate()
 	$GPUParticles2D.process_material.color = color
 	$GPUParticles2D.restart()
