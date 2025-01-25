f9730e2405ab5e93a622a650183049818a1b4903
Konstantin Tretyakov <konstantin.tretjakov@gmail.com>
2025-01-25 20:20:49 +0100

    Three stars on first level, yay!

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -4,6 +4,8 @@ var players: Array[PlayerState]
 
 func _ready() -> void:
 	$Star.star_collected.connect(_on_star_collected)
+	$Star2.star_collected.connect(_on_star_collected)
+	$Star3.star_collected.connect(_on_star_collected)
 
 func _on_star_collected(player_id: int):
 	players[player_id-1].bonus_shots_remaining += 3
