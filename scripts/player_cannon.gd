928d778ffb18331a556cb0662bcf4535347870ec
Konstantin Tretyakov <konstantin.tretjakov@gmail.com>
2025-01-25 18:36:40 +0100

    Make cannons move back and forth

--- scripts/player_cannon.gd
+++ scripts/player_cannon.gd
@@ -11,6 +11,8 @@ const SHAKE_FREQUENCY = 100  # .. divided by 2pi
 # Angle of 0 means horizontal, 90 means vertical
 var angle = 0.0
 var fire_duration = 0.0
+var elapsed_time = 0.0
+@onready var initial_position = position
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,7 +20,10 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	elapsed_time += delta
 	var mirror = 1 if player_id == 1 else -1
+	var x_offset = mirror * (sin(elapsed_time) * 100 + 100)
+	position.x = initial_position.x + x_offset
 	if Input.is_action_pressed("rotate_cw_player_" + str(player_id)):
 		rotate_cannon(delta * ROTATION_SPEED * mirror)
 	elif Input.is_action_pressed("rotate_ccw_player_" + str(player_id)):
@@ -42,5 +47,5 @@ func _process(delta: float) -> void:
 
 
 func rotate_cannon(rotation_angle_change: float) -> void:
-	angle = clamp(angle + rotation_angle_change, -90, 0)
+	angle = clamp(angle + rotation_angle_change, -120, 0)
 	rotation_degrees = angle if player_id == 1 else -angle
