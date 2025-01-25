b28dedacaffc35a32f67e58f8dacf84e4b55b56c
Konstantin Tretyakov <konstantin.tretjakov@gmail.com>
2025-01-25 15:36:02 +0100

    Wubwub sound now changes pitch in addition to volume when you hold fire.

--- scripts/player_cannon.gd
+++ scripts/player_cannon.gd
@@ -28,16 +28,17 @@ func _process(delta: float) -> void:
 		fire_duration = 0.0
 		$Wubwub.play()
 		$Wubwub.volume_db = fire_duration*10
+		$Wubwub.pitch_scale = 1.0
 	if Input.is_action_pressed("shoot_player_" + str(player_id)):
 		fire_duration += delta
 		$Wubwub.volume_db = fire_duration*10
+		$Wubwub.pitch_scale = (1.0 + clamp(fire_duration/2, 0, 1))
 		offset = Vector2(0, 45) + SHAKE_MAGNITUDE*fire_duration * Vector2(sin(SHAKE_FREQUENCY*fire_duration), cos(SHAKE_FREQUENCY*1.1*fire_duration))
 	if Input.is_action_just_released("shoot_player_" + str(player_id)):
 		bubble_fired.emit(player_id, fire_duration)
 		# This is the initial value from the UI
 		offset = Vector2(0, 45)
 		$Wubwub.stop()
-		$Wubwub.volume_db = 0
 
 
 func rotate_cannon(rotation_angle_change: float) -> void:
