2e8e4009361b98c62cb10e045f2660a62d718070
Karolina Flendrich <czaplakaro@gmail.com>
2025-01-26 14:30:26 +0100

    Fix level 3

--- nodes/levels/level_1.gd
+++ nodes/levels/level_1.gd
@@ -3,9 +3,9 @@ extends Node2D
 var players: Array[PlayerState]
 
 func _ready() -> void:
+	$Star.star_collected.connect(_on_star_collected)
 	$Star2.star_collected.connect(_on_star_collected)
 	$Star3.star_collected.connect(_on_star_collected)
-	$Star4.star_collected.connect(_on_star_collected)
 
 func _on_star_collected(player_id: int, is_holy: bool):
 	if is_holy:
