656a595a4632670348f58181075c10ead543b9a8
Valentin Schlattinger <v.schlattinger@gmail.com>
2025-01-26 15:49:26 +0100

    Add trophies and improve game end coop screen

--- nodes/game_end/game_end_coop.gd
+++ nodes/game_end/game_end_coop.gd
@@ -2,6 +2,9 @@ extends Control
 @onready var score_display_label1 = $ScoreDisplayLabel1
 @onready var stars_label = $StarsLabel
 @onready var hint_label = $HintLabel
+@onready var trophy_bronze = $TrophyBronze
+@onready var trophy_silver = $TrophySilver
+@onready var trophy_gold = $TrophyGold
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,21 +13,26 @@ func _ready() -> void:
 	score_display_label1.text=str("%.1f %%" % formatedScoreP1)
 	score_display_label1.set("theme_override_colors/font_color",Color.HOT_PINK)	
 	
-	score_display_label1.position.x = 370
 	stars_label.modulate=Color.HOT_PINK
+	trophy_bronze.visible = false
+	trophy_silver.visible = false
+	trophy_gold.visible = false
 	
-	if(Global.score_p1<0.5):
+	if(Global.score_p1<0.1):
 		stars_label.text="Nice try."
 		hint_label.text = "Try to cover 50 % of the canvas to get a trophy."
 	elif(Global.score_p1<0.6):
+		trophy_bronze.visible = true
 		stars_label.modulate=Color.DARK_ORANGE
 		stars_label.text="Bronze!"
 		hint_label.text = "Try to get 60 % for silver."
 	elif(Global.score_p1<0.7):
+		trophy_silver.visible = true
 		stars_label.modulate=Color.SILVER
 		stars_label.text="Silver!"
 		hint_label.text = "Try to get 70 % for gold."
 	else:
+		trophy_gold.visible = true
 		stars_label.modulate=Color.GOLDENROD
 		stars_label.text="Gold!"
 		hint_label.text = "You are an amazing team!"
