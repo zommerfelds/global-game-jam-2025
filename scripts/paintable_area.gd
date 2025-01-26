600974c602c703414571d35337f2079afd75a3af
Valentin Schlattinger <v.schlattinger@gmail.com>
2025-01-26 12:31:53 +0100

    clean up paintable_area and make borders of the canvas paintable

--- scripts/paintable_area.gd
+++ scripts/paintable_area.gd
@@ -1,34 +1,21 @@
 extends Sprite2D
 
-var border_size = 10
-var area_width = 710
-var area_height = 280
-var splash_size = 20
-var background_color = Color.LAVENDER
 var player_colors = [Color.HOT_PINK, Color.BLUE]
-var image = Image.create(area_width, area_height, false, Image.FORMAT_RGBA8)
 var splash_images = Array()
-
-@onready var area = (area_width - 2 * border_size) * (area_height - 2 * border_size)
+var area_width: int
+var area_height: int
+var area: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	image = texture.get_image()
-	background_color = image.get_pixel(100, 100)
 	area_width = texture.get_size().x
 	area_height = texture.get_size().y
-	#image.fill(Color.DARK_SLATE_BLUE)
-	#image.fill_rect(
-	#	Rect2i(border_size, border_size, area_width - 2*border_size, area_height - 2*border_size),
-	# 	Color.LAVENDER
-	#)
-	texture = ImageTexture.create_from_image(image)
+	area = area_width * area_height
 	splash_images = [
 		Image.load_from_file("res://assets/splashes/splash_01_x64.png"),
 		Image.load_from_file("res://assets/splashes/splash_02_x64.png"),
 		Image.load_from_file("res://assets/splashes/splash_03_x64.png")
 	]
-	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -43,6 +30,7 @@ func paint_splash(position: Vector2, color: Color) -> int:
 	var splash_image = splash_images[0]
 	var splash_width = splash_image.get_width()
 	var splash_height = splash_image.get_height()
+	var image = texture.get_image()
 	var offset = Vector2i(
 		- (splash_width / 2),
 		- (splash_height / 2))
@@ -50,7 +38,6 @@ func paint_splash(position: Vector2, color: Color) -> int:
 	for x in range(splash_width):
 		for y in range(splash_height):
 			var relative_position = Vector2i(x, y) + Vector2i(position) + offset
-			# Don't paint if the pixel already has a non-background color.
 			if is_out_of_bounds(relative_position.x, relative_position.y, image):
 				continue
 			var offset_vector = Vector2(x, y) - splash_image.get_size()/2.0
@@ -61,7 +48,10 @@ func paint_splash(position: Vector2, color: Color) -> int:
 			if (alpha == 0):
 				continue
 			var color_before = image.get_pixel(relative_position.x, relative_position.y)
-			if isColorSameIgnoringAlpha(color_before, background_color):
+			if not (
+				isColorSameIgnoringAlpha(color_before, player_colors[0]) or
+				 isColorSameIgnoringAlpha(color_before, player_colors[1])
+				):
 				image.set_pixel(relative_position.x, relative_position.y, Color(color, alpha))
 				num_pixels_painted += 1
 			elif isColorSameIgnoringAlpha(color_before, color):
