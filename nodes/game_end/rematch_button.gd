93fa795a4db11d3cf5ad53d4af61e12731085300
Yanick Senn <ys+github@yanicksenn.com>
2025-01-26 12:46:52 +0100

    Color everywhere... wow

--- nodes/game_end/rematch_button.gd
+++ nodes/game_end/rematch_button.gd
@@ -12,4 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed() -> void:
+	await $"../Colorizer".spawn_splashes(200)
 	get_tree().change_scene_to_file("res://nodes/main_scene.tscn")
---
+++ scripts/colorizer.gd
@@ -0,0 +1,31 @@
+extends Node
+
+var splash_image = Image.load_from_file("res://assets/splashes/splash_01_x64.png")
+var splash_effect = load("res://nodes/splash_effect.tscn")
+var colors = [Color.HOT_PINK, Color.BLUE]
+	
+func spawn_splashes(amount: int, interval: float = 0.05) -> void:
+	var spawned = 0
+	var batch_size = 8
+	while spawned < 200:
+		for i in range(batch_size):
+			_spawn_splash_somewhere()	
+			spawned += 1
+		await get_tree().create_timer(interval).timeout
+
+func _spawn_splash_somewhere() -> void:
+	var x = randi_range(0, 800)
+	var y = randi_range(0, 450)
+	
+	var color = colors.pick_random()
+	var sprite = Sprite2D.new()
+	sprite.position = Vector2(x, y)
+	sprite.texture = ImageTexture.create_from_image(splash_image)
+	sprite.global_rotation = randf() * 2 * PI
+	sprite.modulate = color
+	add_child(sprite)
+	
+	var splash = splash_effect.instantiate()
+	splash.color = color
+	splash.position = Vector2(x, y);
+	add_child(splash)
